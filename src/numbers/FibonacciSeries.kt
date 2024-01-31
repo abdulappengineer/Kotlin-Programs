3674cb96d98184c235600dc3b2f0d54400ae5f83
abdul <abduluptu@gmail.com>
2024-01-31 13:09:00 +0530

    Initial Commit

--- src/numbers/FibonacciSeries.kt
+++ src/numbers/FibonacciSeries.kt
@@ -2,32 +2,26 @@ package numbers
 
 import java.util.*
 
-class FibonacciSeries {
+fun generateFibonacciSeries(num: Int) {
+    var i = 1
+    var a = 0
+    var b = 1
+    var sum: Int
 
-    fun generateFibonacciSeries(num: Int) {
-        var i = 1
-        var a = 0
-        var b = 1
-        var sum: Int
+    println("Fibonacci Series : ")
 
-        println("Fibonacci Series : ")
-
-        while (i <= num) {
-            print("$a ")
-            sum = a + b
-            a = b
-            b = sum
-            i++
-        }
+    while (i <= num) {
+        print("$a ")
+        sum = a + b
+        a = b
+        b = sum
+        i++
     }
-
 }
 
 fun main() {
     val scanner = Scanner(System.`in`)
     print("Enter the number: ")
     val num = scanner.nextInt()
-
-    val fibonacciObject = FibonacciSeries()
-    fibonacciObject.generateFibonacciSeries(num)
+    generateFibonacciSeries(num)
 }
